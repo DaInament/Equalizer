7360177a3e19c4f87889f6fa5a67ded75530c076
nikitarysin <rysinna@sibintek.ru>
2019-04-10 22:02:02 +0300

    Initial Commit

---
+++ Equalizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Equalizer
+//
+//  Created by Никита Рысин on 10/04/2019.
+//  Copyright © 2019 Rysin Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
